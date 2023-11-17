dd8fac1b0c25565a3dd2be2246a374fbe32552c5
Ly Ha <lythituyetha@gmail.com>
2023-11-17 17:06:15 -0600

    process_applicant update

--- src/applicant_processor.py
+++ src/applicant_processor.py
@@ -4,5 +4,7 @@ from src.applicant import Applicant
 def no_search_criteria(application): 
     return (Status.PASS, "nothing to check")
 
-def process_applicant(applicant=Applicant(False), criteria=no_search_criteria):
+def process_applicant(applicant=Applicant(False), criteria=()):
+    if not criteria:
+        return no_search_criteria(applicant)
     return criteria(applicant)
