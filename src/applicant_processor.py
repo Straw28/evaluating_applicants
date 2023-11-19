b222b2c8101c4de0d2be6b89051c66ce57cf0915
Ly Ha <lythituyetha@gmail.com>
2023-11-18 19:02:18 -0600

    applying feedback

--- src/applicant_processor.py
+++ src/applicant_processor.py
@@ -4,7 +4,10 @@ from src.applicant import Applicant
 def no_search_criteria(application): 
     return (Status.PASS, "nothing to check")
 
-def process_applicant(applicant=Applicant(False), criteria=()):
+
+def process_applicant(applicant=(), criteria=()):
+    if not applicant:
+        pass
     if not criteria:
         return no_search_criteria(applicant)
     return criteria(applicant)
--- src/criteria/applicant_employment_status.py
+++ src/criteria/applicant_employment_status.py
@@ -2,5 +2,6 @@ from src.application_status import Status
 # from src.applicant import Applicant
 
 def evaluate_application(application): 
-    return (Status.PASS, "Applicant has had previous employment.") if application.is_employed else (Status.FAIL, "Applicant has no previous employment.")
+    return (Status.PASS, "Applicant has had previous employment.")\
+        if application.is_employed else (Status.FAIL, "Applicant has no previous employment.")
  
