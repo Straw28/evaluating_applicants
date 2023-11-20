ba648000540d292b06b267ce7379336c5e2f14ad
Ly Ha <lythituyetha@gmail.com>
2023-11-19 19:57:22 -0600

    pushing in next test

--- src/applicant.py
+++ src/applicant.py
@@ -3,3 +3,4 @@ from dataclasses import dataclass
 @dataclass
 class Applicant:
     is_employed: bool=False
+    has_criminal_record: bool=False
--- test/test_applicant_processor.py
+++ test/test_applicant_processor.py
@@ -3,6 +3,7 @@ from src.applicant_processor import process_applicant
 from src.application_status import Status
 from src.applicant import Applicant
 from src.criteria.applicant_employment_status import evaluate_application as check_employment
+from src.criteria.applicant_
 
 class TestApplicant(unittest.TestCase):
     def setUp(self):
@@ -12,6 +13,7 @@ class TestApplicant(unittest.TestCase):
         self.pass_no_check = (Status.PASS, "nothing to check")
         self.pass_employed = (Status.PASS, "Applicant has had previous employment.")
         self.fail_employed = (Status.FAIL, "Applicant has no previous employment.")
+        self.pass_no_criminal_record = (Status.PASS, "Applicant has had no criminal record.")
 
 
     def test_canary(self):
@@ -28,3 +30,7 @@ class TestApplicant(unittest.TestCase):
     def test_one_criteria_employment_status_returns_expected_fail(self):
   
         self.assertEqual(process_applicant(self.application_with_no_employment, check_employment), self.fail_employed)
+
+    def test_two_criteria_employment_and_criminal_status_returns_expected_pass(self):
+  
+        self.assertEqual(process_applicant(self.application_with_no_employment, [check_employment, check_criminal_record]), self.pass_no_criminal_record)
