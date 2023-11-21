51635c9867c23337ef6838167e2aeeb9d02dc4b8
Ly Ha <lythituyetha@gmail.com>
2023-11-20 20:34:50 -0600

    trying a zip

--- src/applicant_processor.py
+++ src/applicant_processor.py
@@ -3,14 +3,10 @@ from src.application_status import Status
 def no_search_criteria(application): 
     return (Status.PASS, "nothing to check")
 
-def collect_passed_criteria_messages(applicant, *criteria):
-    return [message for status, message in [criterion(applicant) for criterion in criteria] if status == Status.PASS]
-
-def collect_failed_criteria_messages(applicant, *criteria):
-    return [message for status, message in [criterion(applicant) for criterion in criteria] if status == Status.FAIL]
 
 def process_applicant(applicant, *criteria):
     if not criteria:
         return no_search_criteria(applicant)
 
-    return (Status.FAIL, " ".join(collect_failed_criteria_messages(applicant, *criteria))) if len(collect_passed_criteria_messages(applicant, *criteria)) != len(criteria) else (Status.PASS, " ".join(collect_passed_criteria_messages(applicant, *criteria)))
+    passed, messages = zip(*(criterion(applicant) for criterion in criteria))
+    return (Status.FAIL, " ".join(messages)) if any(status == Status.FAIL for status in passed) else (Status.PASS, " ".join(messages))
