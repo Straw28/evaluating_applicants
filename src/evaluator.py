b99029df5a19748c62d64aca93a511a3277737cc
Straw28 <101749695+Straw28@users.noreply.github.com>
2023-11-29 19:34:20 -0600

    feedback for evaluator ui

--- src/evaluator.py
+++ src/evaluator.py
@@ -27,7 +27,7 @@ def grab_criteria_by_number(user_response):
 def process_applicant_info(question: str) -> bool:
         while True:
             try:
-                user_input = input(f"{question} (True/False): ").lower()
+                user_input = input(f"{question} (True/False): ").lower().strip()
                 if user_input in ['true', 'false']:
                     return user_input == 'true'
                 raise ValueError
@@ -51,10 +51,28 @@ def get_application_info():
 def create_application(application_info):
     return Applicant(**application_info)
 
-
-if __name__ == "__main__":
+def add_applicant_valid_response():
+        while True:
+            try:
+                user_input = input("Would you like to process another application? Please enter Yes or No: ").lower().strip()
+                if user_input == 'yes':
+                    return True
+                elif user_input == 'no':
+                    return False
+                raise ValueError
+            except ValueError:
+                print("Invalid input. Please enter Yes or No.")
+                
+if __name__ == "__main__": 
     display_menu('src/criteria')
-    chosen_criteria = grab_criteria_by_number(get_user_criteria())
-    new_applicant = create_application(get_application_info())
+    chosen_criteria = grab_criteria_by_number(get_user_criteria()) 
     criteria_functions = [fetch_criterion(criterion, 'criteria') for criterion in chosen_criteria]
-    print(process_applicant(new_applicant, *criteria_functions))
+    
+    continue_entering_applicants = True
+    
+    while continue_entering_applicants:
+
+        new_applicant = create_application(get_application_info()) 
+        print(process_applicant(new_applicant, *criteria_functions))
+
+        continue_entering_applicants = add_applicant_valid_response()
--- src/evaluator_UI.py
+++ src/evaluator_UI.py
@@ -1,52 +1,78 @@
 from applicant import Applicant
 from applicant_processor import process_applicant
 from fetch_criteria import fetch_criteria
-# from src.fetch_criterion import fetch_criterion
+from fetch_criterion import fetch_criterion
+
 
 def display_menu(path):
-    print("Select criterias to evaluate the following applicant: ")
+    print("Application Evaluation Criteria List: ")
     for menu_num, criterion in enumerate(fetch_criteria(path), start=1):
         print(f"{menu_num}. {criterion}")
 
-def get_response():
+def get_user_criteria():
     while True:
-        user_input = input("Enter your menu selections (comma-separated, type 'exit' to finish): ")
-
-        if user_input == 0:
-            return []
-        
-        selections = user_input.split(',')
         try:
-            response = [int(selection) for selection in selections]
-            return response
-        
+            user_input = input("Choose criteria number(s) separated by commas: ")
+            chosen_menu_numbers = tuple(map(int, user_input.split(',')))
+            
+            return chosen_menu_numbers
         except ValueError:
-            print("Invalid input. Please enter a comma-separated list of numbers.")
+            print("Invalid input. Please enter a valid criteria number from the menu separated by commas.")
 
-def exit_application(choice):
-    return True if choice else False #pass sm else for choice criteria
+def grab_criteria_by_number(user_response):
+    criteria_names = fetch_criteria('src/criteria')
+    chosen_criteria = [criteria_names[menu_num - 1] for menu_num in user_response if 1 <= menu_num <= len(criteria_names)]
+    return tuple(chosen_criteria)
 
-def create_applicant():
-    my_applicant = Applicant()
-    return my_applicant
+def process_applicant_info(question: str) -> bool:
+        while True:
+            try:
+                user_input = input(f"{question} (True/False): ").lower()
+                if user_input in ['true', 'false']:
+                    return user_input == 'true'
+                raise ValueError
+            except ValueError:
+                print("Invalid input. Please enter True or False.")
 
-def set_applicant_attributes(applicant, criteria, value):
-    for criterion in criteria:
-        set_applicant_attributes(new_applicant, criterion, input(f"enter the value of {criterion} (True or False): "))
-    applicant.attribute = value #this isn't gonna work.
+def get_application_info():
+    print("Provide applicant details:")
+    is_employed = process_applicant_info("Is the applicant employed?")
+    has_no_criminal_record = process_applicant_info("Does the applicant have no criminal record?")
+    has_good_credit_record = process_applicant_info("Does the applicant have a good credit record?")
+    has_security_clearance = process_applicant_info("Does the applicant have security clearance?")
 
-def get_application_details():
-    new_applicant = create_applicant('src/criteria')
+    return {
+        "is_employed": is_employed,
+        "has_no_criminal_record": has_no_criminal_record,
+        "has_good_credit_record": has_good_credit_record,
+        "has_security_clearance": has_security_clearance
+    }
 
-    while True:
-        display_menu()
-        criteria = get_response()
-        if exit_application(criteria):
-            break
+def create_application(application_info):
+    return Applicant(**application_info)
 
-        print(process_applicant(new_applicant, criteria))
+def add_applicant_valid_response():
+        while True:
+            try:
+                user_input = input("Would you like to process another application? Please enter Yes or No: ").lower()
+                if user_input == 'yes':
+                    return True
+                elif user_input == 'no':
+                    return False
+                raise ValueError
+            except ValueError:
+                print("Invalid input. Please enter Yes or No.")
+                
+if __name__ == "__main__": 
+    display_menu('src/criteria')
+    chosen_criteria = grab_criteria_by_number(get_user_criteria()) 
+    criteria_functions = [fetch_criterion(criterion, 'criteria') for criterion in chosen_criteria]
+    
+    continue_entering_applicants = True
+    
+    while continue_entering_applicants:
 
+        new_applicant = create_application(get_application_info()) 
+        print(process_applicant(new_applicant, *criteria_functions))
 
-if __name__ == "__main__":
-    get_application_details()
-
+        continue_entering_applicants = add_applicant_valid_response()
